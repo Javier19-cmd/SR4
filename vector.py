2a26ccdb7e6903ed6986a2f89f712579a584fdcd
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-08-02 17:52:06 -0600

    agregando operaciones

--- vector.py
+++ vector.py
@@ -1,8 +1,26 @@
 class V3(object):
     def __init__(self, x, y, z = 0):
+        #Recibiendo una lista de 3 elementos.
         self.x = x
         self.y = y
         self.z = z
 
-    def __repr__(self): 
+    #OVerload de la suma.
+    def __add__(self, other):
+        return V3(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.y
+        )
+
+    #Ovoerload de la resta.
+    def __sub__(self, other):
+        return V3(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.y
+        )
+
+    def __repr__(self): #Overloading de la funcion __repr__
+        #Devuelve una cadena que representa el objeto.
         return("V3(%s, %s, %s)" % (self.x, self.y, self.z))
